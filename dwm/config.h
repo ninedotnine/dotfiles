7a938272964b037e8a58259fc872a904357fa54d
dan soucy <dev@danso.ca>
2015-07-24 14:35:59 -0400

    dwm/config.h: changed width of master window
    now it's exactly 79 chars in urxvt on multivac

--- dwm/config.h
+++ dwm/config.h
@@ -61,7 +61,8 @@ static const Rule rules[] = {
 
 /* layout(s) */
 // static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const float mfact      = 0.5513; /* 79 chars wide for urxvt with inconsolata */
+// static const float mfact      = 0.5513; /* 79 chars wide for urxvt with inconsolata */
+static const float mfact      = 0.4913; /* 79 chars wide for urxvt with inconsolata */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
