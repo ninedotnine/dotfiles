7d8e2a6baf3c9bec3350c66146800964143eca83
dan soucy <dev@danso.ca>
2018-08-11 00:07:50 -0400

    dwm: make main window 119 characters on eddie
    this computer has way better resolution than multivac did!

--- dwm/config_statuscolors.h
+++ dwm/config_statuscolors.h
@@ -99,9 +99,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-// static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-// static const float mfact      = 0.5513; /* 79 chars wide for urxvt with inconsolata */
-static const float mfact      = 0.4913; /* 79 chars wide for urxvt with inconsolata */
+static const float mfact      = 0.5157; /* 119 chars wide for urxvt with inconsolata */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 // static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
